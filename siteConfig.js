cb2f2b607c77493ae1df20d9d88e2c35a1923cda
dax8it <alexcovo@gmail.com>
2020-03-12 08:10:30 -0400

    Update siteConfig.js
    fixed dupe

--- siteConfig.js
+++ siteConfig.js
@@ -1,8 +1,7 @@
 module.exports = {
   name: "AT JIUJITSU NYC",
   shortName: "AT Jiu-Jitsu NYC",
-  description:
-    "Jiujitsu is for Everyone. From our toddler & kids programs to our adult BJJ classes, we offers the best BJJ & martial arts program in Jackson Heights Queens.",
+  description: "Jiujitsu is for Everyone.",
   url: "https://atjiujitsunyc.com", // Domain of your site without prefix!
   prefix: "/",
   author: "atjiujitsunyc",
