86013b1b4dc58382aba834f45153c4fa295e2cf9
dax8it <alexcovo@gmail.com>
2020-03-13 16:36:21 -0400

    removed service worker
    uninstalled gatsby-plugin-offline and added
    gatsby-plugin-remove-serviceworker

--- gatsby-config.js
+++ gatsby-config.js
@@ -176,11 +176,6 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-netlify`,
-    {
-      resolve: `gatsby-plugin-offline`,
-      options: {
-        precachePages: [`/calendar/`, `/contact/`],
-      },
-    },
+    `gatsby-plugin-remove-serviceworker`,
   ],
 }
