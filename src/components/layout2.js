bf3f45ab20b70da01fa833fea7fc8f43780bf4fc
dax8it <alexcovo@gmail.com>
2023-08-30 22:19:15 -0400

    layout 2 test

--- src/components/layout2.js
+++ src/components/layout2.js
@@ -1,9 +1,5 @@
-
 import React, { useState } from "react";
 import { Link } from "gatsby";
-
-// Dummy Footer2 for the sake of example
-const Footer2 = () => <div>Footer2</div>;
 
 const Layout = (props) => {
   const { title, children } = props;
@@ -23,6 +19,14 @@ const Layout = (props) => {
     { label: "About", link: "/about" },
   ];
 
+  const handleToggleNav = () => {
+    setToggleNav(!toggleNav);
+  };
+
+  const handleActiveDropdown = (index) => {
+    setActiveDropdown(index);
+  };
+
   return (
     <div className={`site-wrapper ${toggleNav ? "site-head-open" : ""}`}>
       <header className="site-head" style={{ position: "fixed", width: "100%" }}>
@@ -30,42 +34,40 @@ const Layout = (props) => {
           <a
             className="nav-burger"
             href="#"
-            onClick={() => setToggleNav(!toggleNav)}
+            onClick={handleToggleNav}
             aria-label="Menu"
           >
             🍔
           </a>
-          <nav className="site-head-left">
-            <ul className="nav" role="menu">
-              {menuItems.map((item, index) => (
-                <li key={index} role="none">
-                  {item.subItems ? (
-                    <span
-                      onClick={() => setActiveDropdown(index)}
-                      role="menuitem"
-                    >
-                      {item.label} ▼
-                    </span>
-                  ) : (
-                    <Link to={item.link} role="menuitem">
-                      {item.label}
-                    </Link>
-                  )}
-                  {activeDropdown === index && (
-                    <ul className="dropdown" role="menu">
-                      {item.subItems.map((subItem, subIndex) => (
-                        <li key={subIndex} role="none">
-                          <Link to={subItem.link} role="menuitem">
-                            {subItem.label}
-                          </Link>
-                        </li>
-                      ))}
-                    </ul>
-                  )}
-                </li>
-              ))}
-            </ul>
-          </nav>
+          <ul className="nav">
+            {menuItems.map((item, index) => (
+              <li key={index} role="none">
+                {item.subItems ? (
+                  <span
+                    onClick={() => handleActiveDropdown(index)}
+                    role="menuitem"
+                  >
+                    {item.label} ▼
+                  </span>
+                ) : (
+                  <Link to={item.link} role="menuitem">
+                    {item.label}
+                  </Link>
+                )}
+                {activeDropdown === index && (
+                  <ul className="dropdown" role="menu">
+                    {item.subItems.map((subItem, subIndex) => (
+                      <li key={subIndex} role="none">
+                        <Link to={subItem.link} role="menuitem">
+                          {subItem.label}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </li>
+            ))}
+          </ul>
         </div>
       </header>
       <div className="site-body" style={{ marginTop: "60px" }}>
@@ -76,4 +78,3 @@ const Layout = (props) => {
   );
 };
 
-export default Layout;
