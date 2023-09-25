9913bf7eecdc426b3f7f3746e90b800de2c78b8d
dax8it <alexcovo@gmail.com>
2023-09-25 11:18:30 -0400

    Update index2.js

--- src/pages/index2.js
+++ src/pages/index2.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { graphql, StaticQuery } from "gatsby"
-
+import Img from "gatsby-image"
 import Layout from "../components/layout2"
 import SEO from "../components/seo"
 // import Bio from "../components/bio"
