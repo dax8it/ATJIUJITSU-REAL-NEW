c1fc32f3262d5f776c364137c65c698a455d26ba
dax8it <alexcovo@gmail.com>
2023-10-06 23:24:30 -0400

    Update layout2.js

--- src/components/layout2.js
+++ src/components/layout2.js
@@ -84,7 +84,7 @@ const Layout = props => {
                         <Link to={`/tags/adult-programs`}>Adult Programs</Link>
                   </li>
                   <li className="nav-tags nav-dropdown" role="menuitem">
-                        <Link to={`/tags/Events`}>Events</Link>
+                        <Link to={`/tags/events`}>Events</Link>
                   </li>
                   <li className="nav-tags nav-dropdown" role="menuitem">
                   <a href="https://at-jiujitsu-nyc.maonrails.com/login" 
